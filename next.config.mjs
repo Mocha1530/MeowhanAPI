e1d29423eaf1d58d2a3f348be771f5048b667f06
Mocha <84698300+Mocha1530@users.noreply.github.com>
2025-12-06 04:44:50 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,51 +10,51 @@ const nextConfig = {
     unoptimized: true,
   },
   serverExternalPackages: ['tiktok-live-connector', 'puppeteer-core', '@sparticuz/chromium'],
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     config.externals = config.externals || [];
-  //     config.externals.push({
-  //       'tiktok-live-connector': 'commonjs tiktok-live-connector',
-  //       'puppeteer-core': 'commonjs puppeteer-core',
-  //       '@sparticuz/chromium': 'commonjs @sparticuz/chromium'
-  //     });
-  //   } else {
-  //     config.resolve.fallback = {
-  //       ...config.resolve.fallback,
-  //       fs: false,
-  //       net: false,
-  //       tls: false,
-  //       crypto: false,
-  //       stream: false,
-  //       url: false,
-  //       zlib: false,
-  //       http: false,
-  //       https: false,
-  //       assert: false,
-  //       os: false,
-  //       path: false,
-  //     };
-  //   }
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'tiktok-live-connector': 'commonjs tiktok-live-connector',
+        'puppeteer-core': 'commonjs puppeteer-core',
+        '@sparticuz/chromium': 'commonjs @sparticuz/chromium'
+      });
+    } else {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        url: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        os: false,
+        path: false,
+      };
+    }
 
-  //   return config;
-  // },
+    return config;
+  },
 
-  turbopack: {
-    resolveAlias: {
-      fs: false,
-      net: false,
-      tls: false,
-      crypto: false,
-      stream: false,
-      url: false,
-      zlib: false,
-      http: false,
-      https: false,
-      assert: false,
-      os: false,
-      path: false,
-    },
-  }
+  // turbopack: {
+  //   resolveAlias: {
+  //     fs: false,
+  //     net: false,
+  //     tls: false,
+  //     crypto: false,
+  //     stream: false,
+  //     url: false,
+  //     zlib: false,
+  //     http: false,
+  //     https: false,
+  //     assert: false,
+  //     os: false,
+  //     path: false,
+  //   },
+  // }
 };
 
 export default nextConfig
